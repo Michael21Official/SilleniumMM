848869ce20d55049019bcaaaf3542f892f1f58a8
Michael21Official <115347711+Michael21Official@users.noreply.github.com>
2023-04-12 09:37:36 +0200

    Initial commit

---
+++ src/main/java/dev/michael21official/sillenium/Main.java
@@ -0,0 +1,8 @@
+package dev.michael21official.sillenium;
+
+public class Main {
+    private int test;
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
