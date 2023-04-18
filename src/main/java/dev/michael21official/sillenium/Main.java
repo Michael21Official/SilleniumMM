29a8503369a00728c9c049a8e928c7e904a6821c
Michał Matsalak <115347711+Michael21Official@users.noreply.github.com>
2023-04-18 23:00:11 +0200

    Update Main.java

--- src/main/java/dev/michael21official/sillenium/Main.java
+++ src/main/java/dev/michael21official/sillenium/Main.java
@@ -3,6 +3,7 @@ package dev.michael21official.sillenium;
 public class Main {
     private int test;
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
